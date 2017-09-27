343d2f019b8a46d598199c2db41874611c6b5f39
Sean McGrath <smcgrath@SIDERO.Athlone.Local>
2017-09-27 14:40:34 +0100

    added the login method to user restcontroller

--- src/main/java/com/langon/BackendApplication.java
+++ src/main/java/com/langon/BackendApplication.java
@@ -1,10 +1,22 @@
 package com.langon;
 
+import com.langon.config.JwtFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+
 
 @SpringBootApplication
 public class BackendApplication {
+
+	@Bean
+	public FilterRegistrationBean jwtFilter() {
+		final FilterRegistrationBean registrationBean = new FilterRegistrationBean();
+		registrationBean.setFilter(new JwtFilter());
+		registrationBean.addUrlPatterns("/rest/*");
+		return registrationBean;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
--- src/main/java/com/langon/controller/UserController.java
+++ src/main/java/com/langon/controller/UserController.java
@@ -2,11 +2,17 @@ package com.langon.controller;
 
 import com.langon.model.User;
 import com.langon.service.UserService;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.ServletException;
+import java.util.Date;
+import java.util.Map;
 
 /**
  * Created by smcgrath on 26/09/2017.
@@ -18,8 +24,29 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-    public String login(){
-        return null;
+    @RequestMapping(value="login", method=RequestMethod.POST)
+    public String login(@RequestBody Map<String, String> jsonLoginDetails)throws ServletException{
+        if(jsonLoginDetails.get("username") ==null || jsonLoginDetails.get("password") == null){
+            throw new ServletException("Please fill in username and password");
+        }
+
+        String username = jsonLoginDetails.get("username");
+        String password = jsonLoginDetails.get("password");
+
+        User user = userService.findByUsername((username));
+
+        if(user == null ){
+            throw new ServletException("Username not found.");
+        }
+
+        String pwd = user.getPassword();
+
+        if (!password.equals(pwd)){
+            throw new ServletException("Invalid login. Please check your username and password");
+        }
+
+        return Jwts.builder().setSubject(username).claim("roles", "user").setIssuedAt(new Date())
+                .signWith(SignatureAlgorithm.HS256, "secretkey").compact();
     }
 
     @RequestMapping(value="/register", method = RequestMethod.POST)
